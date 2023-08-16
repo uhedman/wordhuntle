1cf9250271f39e6db5e8926188f922745ff92eb3
Ulises Hedman <ulyhedman@hotmail.com>
2023-08-15 23:37:55 -0300

    header WIP

--- src/App.js
+++ src/App.js
@@ -4,6 +4,41 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>wordhuntle</p>
+        <nav className="App-nav">
+          <a
+            className="App-link"
+            href="https://example.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Sun/Moon
+          </a>
+          <a
+            className="App-link"
+            href="https://example.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Share
+          </a>
+          <a
+            className="App-link"
+            href="https://example.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            History
+          </a>
+          <a
+            className="App-link"
+            href="https://example.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Info
+          </a>
+        </nav>
       </header>
       <div className="App-body"></div>
     </div>
