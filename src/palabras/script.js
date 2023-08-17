0c45a6fbb262e9b933aefce878f6f1629dd2feb4
Ulises Hedman <ulyhedman@hotmail.com>
2023-08-16 21:39:57 -0300

    example matrix

--- src/palabras/script.js
+++ src/palabras/script.js
@@ -2,6 +2,12 @@ const fs = require('fs');  // Requiere el módulo de sistema de archivos
 
 const archivo = 'diccionario.txt';  // Nombre del archivo diccionario
 
+const grid = [['A', 'B', 'C', 'D'],
+              ['H', 'E', 'J', 'E'],
+              ['I', 'O', 'A', 'F'],
+              ['P', 'O', 'N', 'G']]
+// Palabras escondidas: ABEJA, ABEJON, HIPO, DEJAN, EJE, ...
+
 // Lee el contenido del diccionario
 fs.readFile(archivo, 'utf-8', (error, data) => {
   if (error) {
