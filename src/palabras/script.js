32221a0e185ce2f0c875e15c5eaac42a88537963
Ulises Hedman <ulyhedman@hotmail.com>
2023-08-17 11:16:29 -0300

    getWords WIP

--- src/palabras/script.js
+++ src/palabras/script.js
@@ -1,4 +1,5 @@
 const fs = require('fs');  // Requiere el módulo de sistema de archivos
+const { get } = require('http');
 
 const archivo = 'diccionario.txt';  // Nombre del archivo diccionario
 
@@ -8,6 +9,52 @@ const grid = [['A', 'B', 'C', 'D'],
               ['P', 'O', 'N', 'G']]
 // Palabras escondidas: ABEJA, ABEJON, HIPO, DEJAN, EJE, ...
 
+// Direcciones a las casillas adyacentes
+const ways = [[-1, -1], [-1, 0], [-1, 1],
+              [0 , -1],        , [0 , 1],
+              [1 , -1], [1 , 0], [1 , 1]]
+
+function getWords(data) {
+  let words = [];
+
+  // Se itera para cada casilla de la grilla
+  for (let start = 0; start < 16; start++) {
+    let i = start >> 2, j = start % 4;
+
+    // Casillas tiene la palabra que se va creando, y las casillas que la componen
+    let casillas = [grid[i][j], [i, j]];
+    console.log(casillas);
+
+    let warm_start = false, way = 0;
+    while ( ) {
+      // warm_start significa continuar desde el valor guardado
+      if (warm_start) {
+        way = casillas.pop();
+        casillas[0] = casillas[0].slice(0, casillas[0].length - 1);
+        warm_start = false;
+      }
+      else {
+        start = 0;
+      }
+      for (; way < 9; way++) {
+        // checkear que se puedan construir palabras con las letras adyacentes
+      }
+      
+      // Si ninguna casilla pudo continuar una palabra
+      if (way == 9) {
+        // Llamar a backtracking
+        warm_start = true;
+      }
+      else {
+        if (casillas[0].length > 1 /*&& casillas[0] esta en data*/) {
+          words += casillas[0];
+        }
+      }
+    }
+  }
+  return words;
+}
+
 // Lee el contenido del diccionario
 fs.readFile(archivo, 'utf-8', (error, data) => {
   if (error) {
@@ -16,4 +63,5 @@ fs.readFile(archivo, 'utf-8', (error, data) => {
   }
 
   lineas = data.split('\n');  // Guardamos las palabras en un arreglo
+  getWords(data);
 });
