fcbaa4d24194e68bf76527653a5821e9d4aa6647
Ulises Hedman <ulyhedman@hotmail.com>
2023-08-22 16:05:51 -0300

    WIP getwords duplicates some

--- src/palabras/script.js
+++ src/palabras/script.js
@@ -1,12 +1,12 @@
 const fs = require('fs');  // Requiere el módulo de sistema de archivos
 const { get } = require('http');
 
-const archivo = 'diccionario.txt';  // Nombre del archivo diccionario
+const archivo = 'test.txt';  // Nombre del archivo diccionario
 
-const grid = [['A', 'B', 'C', 'D'],
-              ['H', 'E', 'J', 'E'],
-              ['I', 'O', 'A', 'F'],
-              ['P', 'O', 'N', 'G']]
+const grid = [['a', 'b', 'c', 'd'],
+              ['h', 'e', 'j', 'e'],
+              ['i', 'o', 'a', 'f'],
+              ['p', 'o', 'n', 'g']]
 // Palabras escondidas: ABEJA, ABEJON, HIPO, DEJAN, EJE, ...
 
 // Direcciones a las casillas adyacentes
@@ -23,6 +23,17 @@ function searchTuple(list, t) {
   return -1; // No se encontró la tupla con elementos a y b
 }
 
+function searchPrefix(words, prefix) {
+  for (const word of words) {
+    if (word.startsWith(prefix)) {
+      return true; // Se encontró una palabra con el prefijo
+    } else if (word > prefix) {
+      return false; // Ya pasamos de las palabras que podrían tener el prefijo
+    }
+  }
+  return false; // No se encontró ninguna palabra con el prefijo
+}
+
 function getWords(data) {
   let words = [];
 
@@ -35,11 +46,11 @@ function getWords(data) {
 
     let warm_start = false, way, k, l, last;
     while (true) {
-      console.log(casillas);
+      // console.log(casillas);
       // warm_start significa continuar desde el valor guardado
       if (warm_start) {
         // Se analizaron todos los caminos, pasar a la siguiente casilla
-        if (casillas.length < 2) {
+        if (casillas.length < 3) {
           break;
         }
         last = casillas.pop();
@@ -53,22 +64,23 @@ function getWords(data) {
       else {
         way = 0;
       }
-      while (way < 8) {
+      for (; way < 8; way++) {
         k = i + ways[way][0];
         l = j + ways[way][1];
-        if (k >= 0 && l >= 0 && k < 4 && l < 4 && searchTuple(casillas, [k, l]) < 0) {
-          console.log(k, l);
-          break;
+        // Casilla fuera de la matriz
+        if (k < 0 || l < 0 || k > 3 || l > 3) {
+          continue;
         }
-        else {
-          way++;
+        // Casilla ya elegida
+        if (searchTuple(casillas, [k, l]) >= 0) {
+          continue;
         }
-        // if (!check(casillas[0] + grid[i + ways[way][0], j + ways[way][1]])) {
-        //   way++;
-        // }
-        // else {
-        //   break;
-        // }
+        // No existen palabras con ese prefijo
+        if (!searchPrefix(data, casillas[0] + grid[k][l])) {
+          // console.log(casillas[0] + grid[k][l]);
+          continue;
+        }
+        break;
         // checkear que se puedan construir palabras con las letras adyacentes
       }
       
@@ -78,14 +90,15 @@ function getWords(data) {
         warm_start = true;
       }
       else {
-        // if (casillas[0].length > 1 /*&& casillas[0] esta en data*/) {
-        //   words += casillas[0];
-        // }
         i = k;
         j = l;
         // console.log(k, l);
         casillas.push([k, l]);
         casillas[0] += grid[k][l];
+
+        if (casillas[0].length > 1 && data.indexOf(casillas[0]) >= 0) {
+          words.push(casillas[0]);
+        }
       }
     }
   }
@@ -100,5 +113,5 @@ fs.readFile(archivo, 'utf-8', (error, data) => {
   }
 
   lineas = data.split('\n');  // Guardamos las palabras en un arreglo
-  getWords(data);
+  console.log(getWords(lineas));
 });
