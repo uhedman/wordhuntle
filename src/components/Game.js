1c2ebc74d3d6c760836dc485842ebd517c9a4778
Ulises Hedman <ulyhedman@hotmail.com>
2023-08-29 17:12:00 -0300

    game styles

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,12 +1,41 @@
-import '../App.css';
 export {Game}
 
 function Game() {
+  const gameStyles = {
+    display: 'grid',
+    gridTemplateColumns: '1fr',
+    gridTemplateRows: '0.3fr 0.3fr 2.4fr',
+    gap: '0px 0px',
+    gridAutoFlow: 'row',
+    gridTemplateAreas:`
+      "POINTS"
+      "WORD"
+      "GRID"
+    `,
+    gridArea: 'GAME',
+    padding: '20px',
+  }
+
+  const pointsStyles = {
+    gridArea: 'POINTS',
+    backgroundColor: 'blueviolet'
+  }
+
+  const wordStyles = {
+    gridArea: 'WORD',
+    backgroundColor: 'darkslateblue'
+  }
+
+  const gridStyles = {
+    gridArea: 'GRID',
+    backgroundColor: 'darkslategray'
+  }
+
   return (
-    <div className="App-game">
-      <div className="App-points">Here will be the points and words guessed</div>
-      <div className="App-word">Here will be the word construction</div>
-      <div className="App-grid">Here will be the grid of letters</div>
+    <div style={gameStyles}>
+      <div style={pointsStyles}>Here will be the points and words guessed</div>
+      <div style={wordStyles}>Here will be the word construction</div>
+      <div style={gridStyles}>Here will be the grid of letters</div>
     </div>
   )
 }
