c965d7779fc567d91d7455c8607189b10a81e0fb
Ulises Hedman <ulyhedman@hotmail.com>
2023-08-29 17:03:33 -0300

    header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,22 @@
-import '../App.css';
 import { FaSun, FaShareAlt, FaRegClock, FaInfoCircle } from 'react-icons/fa';
 export {Header}
 
 function Header() {
+  let styles = {
+    backgroundColor: '#0a0a23',
+    gridArea: 'HEADER',
+    fontFamily: "'Source Serif 4', sans serif",
+    padding: 15,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    color: 'white',
+    fontSize: 'xx-large',
+    fontWeight: 700,
+  }
+  
   return (
-    <div className="App-header">
+    <div style={styles}>
       <p>wordhuntle</p>
       <nav>
         <ul className="App-links">
