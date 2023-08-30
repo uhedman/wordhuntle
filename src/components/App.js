c8c2a0de565a0f9321ee5b2a678e4ebdbb386e53
Ulises Hedman <ulyhedman@hotmail.com>
2023-08-29 21:43:48 -0300

    app css

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,39 @@
 import '../App.css';
 import {Header} from './Header'
 import {Game} from './Game'
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Game />
-    </div>
-  );
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      color: 'white',
+      backgroundColor: ''
+  }
+  }
+  render () {
+    const appStyles = {
+      color : 'white',
+      backgroundColor: '#1b1b32',
+      display: 'grid',
+      gridTemplateColumns: '1fr 1fr 1fr',
+      gridTemplateRows: '0.1fr 2fr',
+      gap: '0px 0px',
+      gridAutoFlow: 'row',
+      gridTemplateAreas:`
+      "HEADER HEADER HEADER"
+      ". GAME ."
+      `,
+      height: '100vh',
+      width: '100vw',
+    }
+    return (
+      <div style={appStyles}>
+        <Header />
+        <Game />
+      </div>
+    );
+  }
 }
 
 export default App;
