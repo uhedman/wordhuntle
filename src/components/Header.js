e2563a63a12fcef8a723a6bf53924f54ae5fc699
Ulises Hedman <ulyhedman@hotmail.com>
2023-08-30 18:45:13 -0300

    button style to app.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,37 +5,26 @@ import React from 'react';
 
 class Header extends React.Component {
   render () {
-    const buttonStyles = {
-      border: 'none',
-      background: 'none',
-      cursor: 'pointer',
-    }
-    
     return (
       <header id='Header' className={this.props.theme}>
         <p style={{marginRight: 'auto', className: this.props.theme}}>wordhuntle</p>
         <nav>
           <ul style={{listStyle: 'none'}}>
             <li><Theme 
-                  buttonStyles={buttonStyles} 
-                  iconStyles={{fontSize: 'x-large'}}
                   changeTheme={this.props.changeTheme}
                   theme={this.props.theme}
                 /></li>
             <li><Button 
-                  buttonStyles={buttonStyles} 
                   theme={this.props.theme}
-                  icon={<FaShareAlt style={{fontSize: 'x-large'}}/>} 
+                  icon={<FaShareAlt />} 
                 /></li>
             <li><Button 
-                  buttonStyles={buttonStyles} 
                   theme={this.props.theme}
-                  icon={<FaRegClock style={{fontSize: 'x-large'}}/>} 
+                  icon={<FaRegClock />} 
                 /></li>
             <li><Button 
-                  buttonStyles={buttonStyles}
                   theme={this.props.theme}
-                  icon={<FaInfoCircle style={{fontSize: 'x-large'}}/>} 
+                  icon={<FaInfoCircle />} 
                 /></li>
           </ul>
         </nav>
--- src/components/Theme.js
+++ src/components/Theme.js
@@ -5,10 +5,9 @@ class Theme extends React.Component {
   render () {
     return (
       <button 
-        style={this.props.buttonStyles} 
         onClick={this.props.changeTheme}
         className={this.props.theme}>
-        {this.props.theme === 'dark' ? <FaSun style={this.props.iconStyles}/> : <FaMoon style={this.props.iconStyles}/>}
+        {this.props.theme === 'dark' ? <FaSun /> : <FaMoon />}
       </button>
     )
   }
