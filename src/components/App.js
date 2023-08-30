aee4fe1ccf56ebfa117cb2bf734e8981e10f40a3
Ulises Hedman <ulyhedman@hotmail.com>
2023-08-30 18:22:03 -0300

    App style to app.css

--- src/components/App.js
+++ src/components/App.js
@@ -17,22 +17,8 @@ class App extends React.Component {
     }));
   }
   render () {
-    let appStyles = {
-      display: 'grid',
-      gridTemplateColumns: '1fr 1fr 1fr',
-      gridTemplateRows: '0.1fr 2fr',
-      gap: '0px 0px',
-      gridAutoFlow: 'row',
-      gridTemplateAreas:`
-      "HEADER HEADER HEADER"
-      ". GAME ."
-      `,
-      height: '100vh',
-      width: '100vw',
-    }
-
     return (
-      <div style={appStyles} className={this.state.theme}>
+      <div id='App' className={this.state.theme}>
         <Header 
           changeTheme={this.changeTheme} 
           theme={this.state.theme}
