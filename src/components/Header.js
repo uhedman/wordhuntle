572fee95e8732b45807c13a6cf191340b66e84cc
Ulises <84878988+uhedman@users.noreply.github.com>
2023-09-01 22:35:38 +0000

    header restruct

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,29 +7,25 @@ import { History } from './History';
 class Header extends React.Component {
   render () {
     return (
-      <header id='Header'>
-        <p style={{marginRight: 'auto', className: this.props.theme}}>wordhuntle</p>
-        <nav>
-          <ul style={{listStyle: 'none'}}>
-            <li><Theme 
-                  changeTheme={this.props.changeTheme}
-                  theme={this.props.theme}
-                /></li>
-            <li><Share 
-                  setMenu={this.props.setMenu}
-                  theme={this.props.theme}
-                /></li>
-            <li><History 
-                  setMenu={this.props.setMenu}
-                  theme={this.props.theme}
-                /></li>
-            <li><Info 
-                  setMenu={this.props.setMenu}
-                  theme={this.props.theme}
-                /></li>
-          </ul>
-        </nav>
-      </header>
+      <nav id='Header'>
+        <p style={{marginRight: 'auto'}}>wordhuntle</p>
+        <Theme 
+          changeTheme={this.props.changeTheme}
+          theme={this.props.theme}
+        />
+        <Share 
+          setMenu={this.props.setMenu}
+          theme={this.props.theme}
+        />
+        <History 
+          setMenu={this.props.setMenu}
+          theme={this.props.theme}
+        />
+        <Info 
+          setMenu={this.props.setMenu}
+          theme={this.props.theme}
+        />
+      </nav>
     )
   }
 }
