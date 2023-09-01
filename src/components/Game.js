f5a4ff52ca183679042bb81838ed3a0a12a1eee6
Ulises <84878988+uhedman@users.noreply.github.com>
2023-09-01 21:13:34 +0000

    word to app.css

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,6 @@
 export {Game}
 
 function Game() {
-  const wordStyles = {
-    gridArea: 'WORD',
-    backgroundColor: 'darkslateblue'
-  }
-
   const gridStyles = {
     gridArea: 'GRID',
     backgroundColor: 'darkslategray'
@@ -14,7 +9,7 @@ function Game() {
   return (
     <div id="Game">
       <div id="Points">Here will be the points and words guessed</div>
-      <div style={wordStyles}>Here will be the word construction</div>
+      <div id="Word">Here will be the word construction</div>
       <div style={gridStyles}>Here will be the grid of letters</div>
     </div>
   )
