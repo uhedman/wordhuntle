9b061768960fc9d9813b45973a52c55a5c16c4e9
Ulises <84878988+uhedman@users.noreply.github.com>
2023-09-01 21:14:37 +0000

    grid style in app.css

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,16 +1,11 @@
 export {Game}
 
 function Game() {
-  const gridStyles = {
-    gridArea: 'GRID',
-    backgroundColor: 'darkslategray'
-  }
-
   return (
     <div id="Game">
       <div id="Points">Here will be the points and words guessed</div>
       <div id="Word">Here will be the word construction</div>
-      <div style={gridStyles}>Here will be the grid of letters</div>
+      <div id="Grid">Here will be the grid of letters</div>
     </div>
   )
 }
