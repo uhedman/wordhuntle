a00057ce22b873219f4370d3ecd3978eb85a2ad0
Ulises <84878988+uhedman@users.noreply.github.com>
2023-09-01 22:10:29 +0000

    primitive word construction

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,31 +2,45 @@ import React from 'react';
 import { Tile } from "./Tile"
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      word: ''
+    }
+    this.write = this.write.bind(this);
+  }
+
+  write(letter) {
+    this.setState(state => ({
+      word: state.word + letter
+    }));
+  }
+
   render () {
     return (
       <div id="Game">
         <div id="Points">Here will be the points and words guessed</div>
-        <div id="Word">Here will be the word construction</div>
+        <div id="Word">{this.state.word}</div>
         <div id="Grid">
-          <Tile theme={this.props.theme}/>
-          <Tile theme={this.props.theme}/>
-          <Tile theme={this.props.theme}/>
-          <Tile theme={this.props.theme}/>
+          <Tile write={this.write} theme={this.props.theme}/>
+          <Tile write={this.write} theme={this.props.theme}/>
+          <Tile write={this.write} theme={this.props.theme}/>
+          <Tile write={this.write} theme={this.props.theme}/>
           
-          <Tile theme={this.props.theme}/>
-          <Tile theme={this.props.theme}/>
-          <Tile theme={this.props.theme}/>
-          <Tile theme={this.props.theme}/>
+          <Tile write={this.write} theme={this.props.theme}/>
+          <Tile write={this.write} theme={this.props.theme}/>
+          <Tile write={this.write} theme={this.props.theme}/>
+          <Tile write={this.write} theme={this.props.theme}/>
 
-          <Tile theme={this.props.theme}/>
-          <Tile theme={this.props.theme}/>
-          <Tile theme={this.props.theme}/>
-          <Tile theme={this.props.theme}/>
+          <Tile write={this.write} theme={this.props.theme}/>
+          <Tile write={this.write} theme={this.props.theme}/>
+          <Tile write={this.write} theme={this.props.theme}/>
+          <Tile write={this.write} theme={this.props.theme}/>
 
-          <Tile theme={this.props.theme}/>
-          <Tile theme={this.props.theme}/>
-          <Tile theme={this.props.theme}/>
-          <Tile theme={this.props.theme}/>
+          <Tile write={this.write} theme={this.props.theme}/>
+          <Tile write={this.write} theme={this.props.theme}/>
+          <Tile write={this.write} theme={this.props.theme}/>
+          <Tile write={this.write} theme={this.props.theme}/>
         </div>
       </div>
     )
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -3,7 +3,12 @@ import React from 'react';
 class Tile extends React.Component {
     render() {
         return (
-            <div className={"tile " + this.props.theme}>A</div>
+            <button 
+                onClick={() => this.props.write('A')}
+                className={"tile " + this.props.theme}
+            >
+                A
+            </button>
         );
     }
 }
