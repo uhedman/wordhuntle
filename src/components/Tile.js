0e4d0a4963961be90dd61c0d6b36f785dada72ec
Ulises <84878988+uhedman@users.noreply.github.com>
2023-09-01 21:41:16 +0000

    tile to component and theme

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,7 +1,11 @@
-function Tile() {
-  return (
-    <div className="tile"></div>
-  )
+import React from 'react';
+
+class Tile extends React.Component {
+    render() {
+        return (
+            <div className={"tile " + this.props.theme}></div>
+        );
+    }
 }
 
 export {Tile}
