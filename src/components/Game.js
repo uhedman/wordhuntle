b23cad00634d7af8bb8362c1640fc06c6681678f
Ulises <84878988+uhedman@users.noreply.github.com>
2023-09-01 21:31:20 +0000

    primitive grid

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,33 @@
-export {Game}
+import { Tile } from "./Tile"
 
 function Game() {
   return (
     <div id="Game">
       <div id="Points">Here will be the points and words guessed</div>
       <div id="Word">Here will be the word construction</div>
-      <div id="Grid">Here will be the grid of letters</div>
+      <div id="Grid">
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+        
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+
+        <Tile />
+        <Tile />
+        <Tile />
+        <Tile />
+      </div>
     </div>
   )
 }
+
+export {Game}
---
+++ src/components/Tile.js
@@ -0,0 +1,7 @@
+function Tile() {
+  return (
+    <div className="tile"></div>
+  )
+}
+
+export {Tile}
