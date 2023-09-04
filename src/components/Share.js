55a8b63c9e3c63d152d1786f974a893a6c82db4a
Ulises Hedman <ulyhedman@hotmail.com>
2023-09-04 17:55:42 -0300

    copy to clipboard

--- src/components/Share.js
+++ src/components/Share.js
@@ -2,6 +2,24 @@ import React from 'react';
 import { FaShareAlt } from 'react-icons/fa';
 
 class Share extends React.Component {
+	constructor(props) {
+		super(props);
+		this.copy = this.copy.bind(this);
+	}
+
+	copy() {
+		const shareDiv = document.querySelector('.share'); // Selecciona el div con la clase 'share'
+		const textoACopiar = shareDiv.textContent;
+
+		const elementoTemporal = document.createElement('textarea');
+		elementoTemporal.value = textoACopiar;
+		document.body.appendChild(elementoTemporal);
+		elementoTemporal.select();
+		elementoTemporal.setSelectionRange(0, 99999); // Para dispositivos móviles
+		document.execCommand('copy');
+		document.body.removeChild(elementoTemporal);
+	}
+
 	render () {
 		const fechaActual = new Date();
 
@@ -13,10 +31,10 @@ class Share extends React.Component {
 			<div>
 				<h1>Comparte tus resultados</h1>
 				<div className='share'>
-					<p>wordhuntle - {`${dia} ${mes} ${anio}`}</p>
+					<p>wordhuntle - {`${dia} de ${mes} de ${anio}`} </p>
 					<p>Nivel {this.props.level}/8 — {this.props.points} puntos — {this.props.found} palabras</p>
 				</div>
-				<button className='copy'>Copiar</button>
+				<button className='copy' onClick={this.copy}>Copiar</button>
 			</div>
 		)
 
