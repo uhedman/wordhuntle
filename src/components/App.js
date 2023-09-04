3c7f9cc1742b15303bed78afd2b275e072e524fe
Ulises Hedman <ulyhedman@hotmail.com>
2023-09-04 14:20:27 -0300

    setmenu remove state

--- src/components/App.js
+++ src/components/App.js
@@ -24,10 +24,10 @@ class App extends React.Component {
   }
 
   setMenu(data) {
-    this.setState(state => ({
+    this.setState({
       menu: true,
       menuData: data
-    }));
+    });
   }
 
   closeMenu() {
