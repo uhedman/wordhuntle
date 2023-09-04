902edc2a5885a3ebc9fb5e0d5e4134f48e6bd32b
Ulises Hedman <ulyhedman@hotmail.com>
2023-09-04 14:47:00 -0300

    Uppercase display

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -44,7 +44,7 @@ class Tile extends React.Component {
 				onMouseEnter={this.onMouseEnter}
 				className={"tile " + this.props.theme}
 			>
-				{this.props.letter}
+				{this.props.letter.toUppercase()}
 			</button>
 		);
 	}
