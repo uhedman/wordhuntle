99a7bb0ef29943aeabac571b704c5ed3338126c8
Ulises Hedman <uliseshedman@hotmail.com>
2025-04-02 13:29:23 -0300

    tooltip and clear word

--- src/components/Words.js
+++ src/components/Words.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
 
 const Words = () => {
 	const storageFound = useSelector(state => state.storage.found);
-	const found = storageFound.map(word => <p key={word}>{word}</p>);
 
 	return (
-		<div>
-			{found && <div className='d-grid' style={{ gridTemplate: 'auto auto / repeat(2, 1fr)' }}>{found}</div>}
+		<div className='d-grid' style={{ gridTemplate: 'auto auto / repeat(2, 1fr)' }}>
+			{ storageFound.map((word, idx) => (
+				<div key={word}>
+					<OverlayTrigger 
+						trigger="hover"
+						placement='right'
+						overlay={ 
+							<Tooltip id={`${word}-${idx}`}>
+								Buscar definicion en el diccionario.
+							</Tooltip>
+						}
+					>
+						<a
+							className="link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover"
+							href={`https://dle.rae.es/${word}`} 
+							target="_blank" 
+							rel="noopener noreferrer"
+						>
+							{word}
+						</a>
+					</OverlayTrigger>
+				</div>
+			))}
 		</div>
 	);
 }
