8bd098ee159301ec8f15a065170c63089de29ca4
Ulises Hedman <uliseshedman@hotmail.com>
2025-04-02 16:06:43 -0300

    progress bar css

--- src/components/PointsBar.js
+++ src/components/PointsBar.js
@@ -6,21 +6,20 @@ const PointsBar = () => {
   const points = useSelector((state) => state.storage.points);
   const maxPoints = useSelector((state) => state.game.maxPoints);
   const levels = [0, 12.5, 25, 37.5, 50, 62.5, 75, 87.5, 100];
-
   const progress = Math.sqrt(points / maxPoints) * 100;
 
   return (
-    <div style={{ position: 'relative', width: '100%' }}>
-      <ProgressBar now={progress} style={{ height: '.5rem' }}/>
-      <div 
+    <div style={{ position: 'relative', width: '100%', height: '3rem' }}>
+      <ProgressBar now={progress} style={{ height: '.5rem', position: 'absolute', top: '1.25rem', width: '100%' }}/>
+      <div
         className='d-flex justify-content-between align-items-center w-100'
         style={{
           position: 'absolute',
-          top: '-1.25rem',
+          height: '100%',
         }}
       >
-        { levels.map((level, index) => {
-          if (progress < level && progress > level - 12.5) {
+        {levels.map((level, index) => {
+          if (progress < level && progress >= level - 12.5) {
             return (
               <div
                 key={index}
@@ -28,9 +27,10 @@ const PointsBar = () => {
                 style={{
                   width: '3rem',
                   height: '3rem',
-                  backgroundColor: 'inherit',
+                  backgroundColor: 'var(--bs-secondary-bg)',
                   border: '2px solid var(--blue)',
-                  fontWeight: 'bold'
+                  fontWeight: 'bold',
+                  transition: '0.5s'
                 }}
               >
                 {Math.ceil(((level / 100) ** 2) * maxPoints)}
@@ -44,8 +44,9 @@ const PointsBar = () => {
                 style={{
                   width: '1rem',
                   height: '1rem',
-                  backgroundColor: 'inherit',
+                  backgroundColor: 'var(--bs-secondary-bg)',
                   border: '2px solid var(--blue)',
+                  transition: '0.5s',
                 }}
               />
             )
@@ -59,6 +60,7 @@ const PointsBar = () => {
                   height: '1rem',
                   backgroundColor: 'var(--blue)',
                   border: '2px solid var(--blue)',
+                  transition: '0.5s',
                 }}
               />
             )
