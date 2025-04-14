ffee47dd8b2c894393a3afd22933f9910335f74b
Ulises Hedman <84878988+uhedman@users.noreply.github.com>
2025-04-14 15:23:00 +0000

    line svg to div

--- client/src/components/Line.tsx
+++ client/src/components/Line.tsx
@@ -1,13 +1,48 @@
 import { useAppSelector } from "@/hooks";
+import { Pos } from "@/types";
 
-const Line = () => {
-  const order = useAppSelector((state) => state.drag.order);
+type LineSegmentProps = {
+  p1: Pos;
+  p2: Pos;
+};
+
+const LineSegment = ({ p1: [x1, y1], p2: [x2, y2] }: LineSegmentProps) => {
+  const cellWidth = "calc((100% - 3rem) / 4)";
 
   const calcCoord = (value: number) =>
-    `calc(((100% - 3rem) / 4 + 1rem) * ${value} + (100% - 3rem) / 8)`;
+    `calc((${cellWidth} + 1rem) * ${value} + ${cellWidth} / 2 - 1rem)`;
+
+  const startX = calcCoord(x1);
+  const startY = calcCoord(y1);
+
+  const dx = x2 - x1;
+  const dy = y2 - y1;
+  const distance = Math.sqrt(dx * dx + dy * dy);
+  const angleRad = Math.atan2(dy, dx);
 
   return (
-    <svg
+    <div
+      style={{
+        position: "absolute",
+        top: startY,
+        left: startX,
+        width: `calc(2rem + (${cellWidth} + 1rem) * ${distance})`,
+        height: "2rem",
+        backgroundColor: "var(--blue)",
+        opacity: 0.5,
+        borderRadius: "1rem",
+        transform: `rotate(${angleRad}rad)`,
+        transformOrigin: "1rem 50%",
+      }}
+    />
+  );
+};
+
+const Line = () => {
+  const path = useAppSelector((state) => state.drag.path);
+
+  return (
+    <div
       style={{
         position: "absolute",
         top: 0,
@@ -17,24 +52,18 @@ const Line = () => {
         pointerEvents: "none",
       }}
     >
-      {order.slice(1).map((item, index) => {
-        const [y1, x1] = order[index];
-        const [y2, x2] = item;
+      {path.slice(1).map((_, index) => {
+        const [y1, x1] = path[index];
+        const [y2, x2] = path[index + 1];
         return (
-          <line
-            key={index}
-            x1={calcCoord(x1)}
-            y1={calcCoord(y1)}
-            x2={calcCoord(x2)}
-            y2={calcCoord(y2)}
-            stroke="var(--blue)"
-            strokeWidth="32"
-            strokeLinecap="round"
-            opacity={0.5}
+          <LineSegment
+            key={`${x1}-${y1}-${x2}-${y2}`}
+            p1={[x1, y1]}
+            p2={[x2, y2]}
           />
         );
       })}
-    </svg>
+    </div>
   );
 };
 
