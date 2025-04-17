775cc62380834dd3e811307ca6e7c979d1127459
Ulises Hedman <uliseshedman@hotmail.com>
2025-04-17 19:57:45 -0300

    Fix localStorage theme

--- client/src/store/slices/themeSlice.ts
+++ client/src/store/slices/themeSlice.ts
@@ -5,7 +5,7 @@ interface ThemeState {
 }
 
 const initialState: ThemeState = {
-  value: "light",
+  value: localStorage.getItem("theme") as "dark" | "light" ?? "light",
 };
 
 const themeSlice = createSlice({
