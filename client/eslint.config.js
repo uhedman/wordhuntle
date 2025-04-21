d6ad6cf4c97353cff034ec7e26dfcf1b427a7280
Ulises Hedman <uliseshedman@hotmail.com>
2025-04-21 20:12:46 -0300

    Add lint ignore

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -2,9 +2,10 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
+  globalIgnores(["node_modules", "dist"]),
   {
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     plugins: { js },
