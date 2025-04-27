3fc6c0ecd3ffcf8cef4569339d65b5af8cf77f64
Ulises Hedman <uliseshedman@hotmail.com>
2025-04-27 11:14:54 -0300

    Fix line break

--- client/src/features/share/components/Share.tsx
+++ client/src/features/share/components/Share.tsx
@@ -8,20 +8,17 @@ const Share = () => {
   const points = useAppSelector((state) => state.progress.points);
   const level = useAppSelector((state) => state.progress.level);
 
-  const copy = async () => {
-    const shareDiv = document.querySelector("#share");
-    const text = shareDiv?.textContent; // TODO \n
-
-    if (text) {
-      await navigator.clipboard.writeText(text);
-      setCopied(true);
-    }
-  };
-
   const date = new Date();
   const day = date.getDate();
   const month = date.toLocaleString("default", { month: "long" });
   const year = date.getFullYear();
+  const text1 = `wordhuntle - ${day} de ${month} de ${year}`;
+  const text2 = `Nivel ${level}/8 — ${points} puntos — ${found.length} palabras`;
+
+  const copy = async () => {
+    await navigator.clipboard.writeText(`${text1}\n${text2}`);
+    setCopied(true);
+  };
 
   return (
     <>
@@ -31,12 +28,8 @@ const Share = () => {
       <Modal.Body>
         <div className="d-flex flex-column gap-1">
           <div id="share" className="dark rounded p-2">
-            <p className="m-0">
-              wordhuntle - {day} de {month} de {year}
-            </p>
-            <p className="m-0">
-              Nivel {level}/8 — {points} puntos — {found.length} palabras
-            </p>
+            <p className="m-0">{text1}</p>
+            <p className="m-0">{text2}</p>
           </div>
           <Button onClick={copy}>{copied ? "Copiado!" : "Copiar"}</Button>
         </div>
